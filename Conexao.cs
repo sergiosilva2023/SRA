34486b7c3730761f03fcb79a42d2a4577fa90197
Sérgio Silva <150628460+sergiosilva2023@users.noreply.github.com>
2024-04-04 08:29:10 +0100

    Update Conexao.cs

--- Conexao.cs
+++ Conexao.cs
@@ -10,12 +10,9 @@ namespace SRA
 {
     internal class Conexao
     {   // string de conexão local
-        public string conec = "SERVER=localhost; DATABASE=db_sra; UID=root; PWD=; PORT=;";
+        public string conec = "SERVER=; DATABASE=; UID=root; PWD=; PORT=;";
 
-        // string de conexão remota
-        //public string conec = "SERVER=mysql835.umbler.com; DATABASE=db_sra; UID=srauserdb2024; PWD=s((4x)2H]W; PORT=41890;";
-        //public string conec = "SERVER=5.135.143.184; DATABASE=restaur2_db_sra; UID=restaur2_user_dbsra; PWD=dbsra2024sistema; PORT=;";
-
+        
         // variavél de ligação
         public MySqlConnection con = null;
 
